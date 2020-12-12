7cb833017983605b68c698d543c05ce1c3aa5df1
Evor <delnyavor.com>
2020-12-12 17:56:06 +0000

    api communication working

---
+++ lib/pages/results.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:request_tracker/controller/controller.dart';
+import 'package:request_tracker/models/feedback_form.dart';
+
+class ResultsPage extends StatefulWidget {
+  @override
+  _ResultsPageState createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  List<FeedbackForm> feedbackItems = <FeedbackForm>[];
+
+  // Method to Submit Feedback and save it in Google Sheets
+
+  @override
+  void initState() {
+    super.initState();
+
+    FormController().getFeedbackList().then((feedbackItems) {
+      setState(() {
+        this.feedbackItems = feedbackItems;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: feedbackItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Row(
+              children: <Widget>[
+                Icon(Icons.person),
+                Expanded(
+                  child: Text(
+                      "${feedbackItems[index].name} (${feedbackItems[index].email})"),
+                )
+              ],
+            ),
+            subtitle: Row(
+              children: <Widget>[
+                Icon(Icons.message),
+                Expanded(
+                  child: Text(feedbackItems[index].feedback),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
